659a8c1214b20afe4c30c6094a3205ccdfefa42b
Eliott McKenzie <e4991m@gmail.com>
2025-03-26 20:11:52 +0000

    boilerplate and basic cube setup

---
+++ main.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+if (WebGL.isWebGL2Available()) {
+
+  // Initiate function or other initializations here
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.0;
+    renderer.render(scene, camera);
+  }
+
+
+  renderer.setAnimationLoop(animate);
+
+} else {
+
+  const warning = WebGL.getWebGL2ErrorMessage();
+  document.getElementById('container').appendChild(warning);
+
+}
+
