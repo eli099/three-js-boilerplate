d0baca61913c8a81131c821e398b8bec0b480ec4
Eliott McKenzie <e4991m@gmail.com>
2025-03-27 00:46:30 +0000

    added texture the longer way

--- main.js
+++ main.js
@@ -18,6 +18,11 @@ if (WebGL.isWebGL2Available()) {
   camera.position.set(0, 0, 100);
   camera.lookAt(0, 0, 0);
 
+  // move around
+  const controls = new OrbitControls(camera, renderer.domElement);
+  camera.position.z = 5;
+  controls.update();
+
   const scene = new THREE.Scene();
 
   //create a blue LineBasicMaterial
@@ -48,33 +53,51 @@ if (WebGL.isWebGL2Available()) {
 
   scene.add(line);
 
-  const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-  const materialTwo = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+  const image = new Image()
+  const texture = new THREE.Texture(image)
+  texture.colorSpace = THREE.SRGBColorSpace
+
+  image.onload = () => {
+    texture.needsUpdate = true
+  }
+
+  image.src = 'assets/cheeseMap.jpg'
+
+
+  const cubeGeometry = new THREE.BoxGeometry(7, 7, 7);
+  const materialTwo = new THREE.MeshBasicMaterial({ map: texture });
   const cube = new THREE.Mesh(cubeGeometry, materialTwo);
   scene.add(cube);
+
+  // load a texture, set wrap mode to repeat
+  // const texture = new THREE.TextureLoader().load("assets/cheeseMap.jpg");
+  // texture.wrapS = THREE.RepeatWrapping;
+  // texture.wrapT = THREE.RepeatWrapping;
+  // texture.repeat.set(4, 4);
 
   // ! TEST
   // const something = document.getElementById("info")
   // const texty = new CSS3DRenderer({ something })
   // document.body.appendChild(texty.domElement);
 
-  const renderer2 = new CSS3DRenderer();
-      document.body
-        .getElementById("info")
-        .appendChild(renderer2.domElement);
+  // const renderer2 = new CSS3DRenderer();
+  //     document.body
+  //       .getElementById("info")
+  //       .appendChild(renderer2.domElement);
 
   renderer.render(scene, camera);
-  renderer2.render(scene, camera);
+  // renderer2.render(scene, camera);
 
   // ? Animate cube
   function animate() {
     cube.rotation.x += 0.0;
-    cube.rotation.y += 0.0;
-    cube.rotation.z += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.0;
 
     line.rotation.x += 0.0;
     line.rotation.y += 0.0;
-    line.rotation.z += 0.01;
+    line.rotation.z += 0.0;
     renderer.render(scene, camera);
   }
 
